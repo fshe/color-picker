f7fe9a610934a326c8886f4c722261fb9b33dd49
Frank She <33668602+fshe@users.noreply.github.com>
2020-01-27 00:12:08 -0800

    Updates

--- precache-manifest.69d04ea5fdddceb3ae0a6fbefc96c532.js
+++ precache-manifest.69d04ea5fdddceb3ae0a6fbefc96c532.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef73387019452fd555f6bc83e1f96572",
+    "revision": "399d1d5582cd81aac81ca6d1e5b052c3",
     "url": "/color-picker/index.html"
   },
   {
-    "revision": "9c2e39e17dd7afb836c9",
+    "revision": "c6a7a45d0909aa06528a",
     "url": "/color-picker/static/css/main.d8628f21.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/color-picker/static/js/2.3d1b4393.chunk.js"
   },
   {
-    "revision": "9c2e39e17dd7afb836c9",
-    "url": "/color-picker/static/js/main.02387f2f.chunk.js"
+    "revision": "c6a7a45d0909aa06528a",
+    "url": "/color-picker/static/js/main.d29adaf1.chunk.js"
   },
   {
     "revision": "a7d8e61640eb3f8830d1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-picker/precache-manifest.05cbfb1ed149a5d253b93e3856d09687.js"
+  "/color-picker/precache-manifest.69d04ea5fdddceb3ae0a6fbefc96c532.js"
 );
 
 self.addEventListener('message', (event) => {
