1bca84b20c4029bd67b9dd047c427fd920e37570
Frank She <33668602+fshe@users.noreply.github.com>
2020-01-26 22:19:57 -0800

    Updates

---
+++ precache-manifest.adc4057c2ebffda3ce2e0dcfee0469ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0c5e8701379bc9aa32abb65fba090e",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "915d8a40da0373a42a95",
+    "url": "/color-picker/static/css/main.d8628f21.chunk.css"
+  },
+  {
+    "revision": "823093cafb2656e60843",
+    "url": "/color-picker/static/js/2.765fffed.chunk.js"
+  },
+  {
+    "revision": "915d8a40da0373a42a95",
+    "url": "/color-picker/static/js/main.34cac3a9.chunk.js"
+  },
+  {
+    "revision": "a7d8e61640eb3f8830d1",
+    "url": "/color-picker/static/js/runtime~main.3187d74d.js"
+  }
+]);
